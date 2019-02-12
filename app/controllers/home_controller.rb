7dae689e576a09a3f0e016b0a1309dc9b615918a
cnacir <cknacir@hotmail.com>
2019-02-11 19:27:49 -0500

    finished styling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
 	skip_before_action :authenticate_user!, only: [:index]
   def index
-		if current_user
-			redirect_to rails_admin_path
-		end
   end
 end
