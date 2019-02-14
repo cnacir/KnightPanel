2115ea94182e17725399196045891ad29d0ae054
cnacir <cknacir@hotmail.com>
2019-02-14 13:13:56 -0500

    fixed booleans

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
 
   protected
     def configure_permitted_parameters
-       devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :age, :salary])
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :age, :salary, :instructor, :student])
+			 devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :age, :salary, :instructor, :student])
     end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+	config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Show full error reports.
   config.consider_all_requests_local = true
@@ -56,7 +57,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
 	config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-	
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
